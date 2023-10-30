6e27731276527207a417b2dff4bac4f7f61b1b07
Kacper Dziadkowiec <kacper.dziadkowiec@edu.uekat.pl>
2023-10-30 12:39:44 +0100

    :

---
+++ Zadanie 2a.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+def wyswietl_imiona(imiona):
+    for imie in imiona:
+        print(imie)
+
+imiona = ["Anna", "Bartek", "Karolina", "Piotr", "Marta"]
+wyswietl_imiona(imiona)
+
---
+++ Zadanie 2b.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 30 11:59:00 2023
+
+@author: student
+"""
+#Zadanie 2b
+def pomnoz_przez_2_for(lista):
+    for i in range(len(lista)):
+        lista[i] *= 2
+    return lista
+liczby = [4, 5, 6, 7, 8]
+wynik = pomnoz_przez_2_for(liczby)
+print(wynik)
+
+
+def pomnoz_przez_2_list_comprehension(lista):
+    return [x * 2 for x in lista]
+liczby = [1, 2, 3, 4, 5]
+wynik = pomnoz_przez_2_list_comprehension(liczby)
+print(wynik)
